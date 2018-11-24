6d63259c9a0ad6c00b124a19550730752c537d0f
versx <versefx@gmail.com>
2018-11-24 14:42:02 -0800

    Fix timestamp issue

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ var server = app.listen(port, function() {
 function formatTime(timestamp) {
 	// Create a new JavaScript Date object based on the timestamp
 	// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-	var date = new Date(unix_timestamp*1000);
+	var date = new Date(timestamp*1000);
 	// Hours part from the timestamp
 	var hours = date.getHours();
 	// Minutes part from the timestamp
