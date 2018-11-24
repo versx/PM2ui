b6d9934e9aaf2b8d12a2b7e1a6e6e9b573f93f76
versx <versefx@gmail.com>
2018-11-24 12:46:51 -0800

    More table columns

--- index.js
+++ index.js
@@ -65,6 +65,8 @@ app.get('/test', function(req, res) {
 				<th>` + element.pid + `</th>
 				<th>` + element.cpu + `%</th>
 				<th>` + (element.mem / 1024) + `KB</th>
+				<th>` + element.uptime + `</th>
+				<th>` + element.status + `</th>
 				<th>
 					<div class="btn-group" role="group" aria-label="...">
 						<a class="btn btn-success btn-default" href="/start/` + element.name + `" role="button">Start</a>
